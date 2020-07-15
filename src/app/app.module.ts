ff74e2371e0e80129f0122dd33636881dee90dc4
RSabro <rsabro@users.noreply.github.com>
2020-07-15 12:53:14 -0300

    Initial Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { AuthService } from './login/auth.service';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ActivatedRoute,
+    Observable
+  ],
+  providers: [AuthService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/login/auth.service.ts
@@ -0,0 +1,22 @@
+import { Usuario } from './usuario';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private usuarioAutenticado: boolean = false;
+
+  constructor(private router: Router) { }
+
+  fazerLogin(usuario: Usuario){
+    if(usuario.nome === 'usuario@teste.com' && usuario.senha === '12345'){
+      this.usuarioAutenticado = true;
+      this.router.navigate(['/']);
+    }else{
+      this.usuarioAutenticado = false;
+    }
+  }
+}
